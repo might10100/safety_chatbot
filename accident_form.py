db8aeb9849f37a8761a6599c2f9e1e82104c3356
선정수 <sun@seonjeongsuui-MacBookPro.local>
2026-06-02 17:04:21 +0900

    fix: 현장명 행 열 너비 합산 오류 수정

--- accident_form.py
+++ accident_form.py
@@ -185,7 +185,7 @@ def save_accident_form_pdf(acc: dict, ai_content: str = "",
          _p(acc.get("writer_name",""), align="CENTER"),
          _p("(인)", align="CENTER")],
     ]
-    sc = [W*0.13, W*0.22, W*0.07, W*0.07, W*0.1, W*0.09, W*0.17, W*0.07]
+    sc = [W*0.13, W*0.22, W*0.07, W*0.07, W*0.1, W*0.09, W*0.17, W*0.15]
     s_t = Table(site_data, colWidths=sc, rowHeights=[10*mm])
     s_t.setStyle(_ts(("ALIGN",(1,0),(1,0),"LEFT")))
     story.append(s_t)
